27dd706503d370228db027c707ca2e7f143e2f8b
Akhilnagavelli <akhilkumarnagavelli2003@gmail.com>
2025-01-27 22:56:06 +0530

    ATM

---
+++ ATM/src/com/java/projects/AtmOperationInterf.java
@@ -0,0 +1,8 @@
+package com.java.projects;
+
+public interface AtmOperationInterf {
+	public void viewBalance();
+	public void withdrawAmount(double withdrawAmount);
+	public void depositAmount(double depositAmount);
+	public void viewMiniStatement();
+}
